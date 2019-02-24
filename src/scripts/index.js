04cfb880e226ca0db779b7ea983e92a4083f5150
RCLARK4958 <RICKDCLARK48@GMAIL.COM>
2019-02-24 11:52:38 -0700

    equality_operators

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,13 @@
 console.log( ( 8 > 54) ? "yes" : "no");
+let year = 1964;
+year += 10;
+console.log(year);
+
+let var1 = "23";
+let var2 = 23;
+
+if (var1 == var2) {
+    console.log(true);
+} else {
+    console.log(false);
+}
